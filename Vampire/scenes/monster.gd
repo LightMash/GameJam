ca4e02eb64f32b7202751e0e2a5d135d06a68e4e
ryankhaled <kh.ryan.06@gmail.com>
2026-01-31 11:25:07 -0500

    monster ai

--- Vampire/scenes/monster.gd
+++ Vampire/scenes/monster.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
-@export var movement_speed =20.0
+@export var movement_speed: float = 90.0
 
-@onready var player = get_tree().get_first_node_in_group("player")
+@onready var player: Player = get_tree().get_first_node_in_group("player") as Player
 
-func_physics_process(_delta):
-	var direction + global_position.direction_to(player_global position)
-	velocity = direction *movement_speed
+func _physics_process(delta: float) -> void:
+	if player == null:
+		player = get_tree().get_first_node_in_group("player") as Player
+		if player == null:
+			velocity = Vector2.ZERO
+			return
+
+	var direction: Vector2 = global_position.direction_to(player.global_position) # normalized
+	velocity = direction * movement_speed
 	move_and_slide()
