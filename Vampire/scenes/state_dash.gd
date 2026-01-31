aacdf0e9b704685056c3c81b7c45f80e787ed635
Nehir Bozdogan <nehir.bozdgn@gmail.com>
2026-01-31 17:28:33 -0500

    player dash

--- Vampire/scenes/state_dash.gd
+++ Vampire/scenes/state_dash.gd
@@ -1,4 +1,49 @@
+<<<<<<< Updated upstream
 extends Node
+=======
+class_name State_Dash extends State
+
+@export var dash_speed: float = 350
+@export var dash_duration: float = 0.7
+@export var dash_cooldown: float = 4
+
+
+@onready var idle: State_Idle = $"../idle"
+@onready var walk: State = $"../walk"
+@onready var hurt_box: Area2D = $"../../Interactions/HurtBox"  # adjust path if needed
+
+var dash_dir: Vector2 = Vector2.ZERO
+var time_left: float = 0.0
+var can_dash: bool = true
+
+func Enter() -> void:
+	if not can_dash:
+		return
+
+	can_dash = false
+	time_left = dash_duration
+
+	
+	if player.direction != Vector2.ZERO:
+		player.setDirection()  
+		dash_dir = player.direction.normalized()
+	else:
+		dash_dir = player.cardinal_direction.normalized()
+
+	
+	player.UpdateAnimation("dash")
+
+	
+	if is_instance_valid(hurt_box):
+		hurt_box.monitoring = false
+
+	
+	_start_cooldown()
+
+func Exit() -> void:
+
+	player.velocity = Vector2.ZERO
+>>>>>>> Stashed changes
 
 
 # Called when the node enters the scene tree for the first time.
--- Vampire/scripts/state_idle.gd
+++ Vampire/scripts/state_idle.gd
@@ -2,6 +2,7 @@ class_name State_Idle extends State
 
 @onready var walk : State = $"../walk"
 @onready var attack: Node = $"../attack"
+@onready var dash: Node = $"../dash"
 
 
 func Enter() -> void: 
@@ -21,6 +22,8 @@ func Physics( _delta : float) -> State:
 	return null
 	
 func HandleInput( _event: InputEvent ) -> State: 
+	if _event.is_action_pressed("dash"):
+		return dash
 	if _event.is_action_pressed("attack"):
 		return attack
 	return null
