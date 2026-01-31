ff4907b98d8313b92a5cac78ebc6ecdb150faf01
Nehir Bozdogan <nehir.bozdgn@gmail.com>
2026-01-31 11:31:42 -0500

    debug

--- Vampire/GeneralNodes/HurtBox/hurt_box.gd
+++ Vampire/GeneralNodes/HurtBox/hurt_box.gd
@@ -11,5 +11,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 	
-	func AreaEntered(area : Area2D) -> void:
-		pass
+func AreaEntered(area : Area2D) -> void:
+	if area is HitBox:
+		area.TakeDamage(damage)
+	pass
