a1bf8b74d9052a56afb401b5a1b504917266c098
Yuioytffhio <hiba.tal05@gmail.com>
2026-02-01 03:50:06 -0500

    Dice update

--- Vampire/ui/dice_roll.gd
+++ Vampire/ui/dice_roll.gd
@@ -1,42 +1,42 @@
-extends Sprite2D
+extends Sprite2D   # THIS is the rolling dice sprite
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var roll_anim: AnimationPlayer = $AnimationPlayer
+@onready var faces_parent: Node = $Sprite2D2   # final dice faces container
 
 var last_day := -1
 var dice_result := 1
 
-
-func _ready() -> void:
+func _ready():
 	NightCycleManager.time_tick.connect(_on_time_tick)
-	animation_player.animation_finished.connect(_on_animation_finished)
-	
-	# Show initial dice face (face 1)
-	show_dice_face(1)
+	roll_anim.animation_finished.connect(_on_roll_finished)
 
-func _on_time_tick(day: int, hour: int, minute: int) -> void:
-	# Only trigger at the START of a new day
+	_show_face(1)
+
+func _on_time_tick(day: int, hour: int, minute: int):
 	if day != last_day:
 		last_day = day
 		start_dice_roll()
 
-func start_dice_roll() -> void:
-	# Decide the result FIRST
+func start_dice_roll():
 	dice_result = randi_range(1, 6)
-	
-	# Play roll animation
-	animation_player.play("dice")
 
-func _on_animation_finished(anim_name: String) -> void:
+	# hide FINAL face during roll
+	faces_parent.visible = false
+
+	# stop any previous face animation
+	for ap in faces_parent.get_children():
+		ap.stop()
+
+	roll_anim.play("dice")
+
+func _on_roll_finished(anim_name: String):
 	if anim_name == "dice":
-		show_final_dice()
+		faces_parent.visible = true
+		_show_face(dice_result)
 
-func show_final_dice() -> void:
-	# Show the final dice result
-	show_dice_face(dice_result)
+func _show_face(face: int):
+	var ap_name = "animationplayer" + str(face)
+	var face_anim: AnimationPlayer = faces_parent.get_node(ap_name)
 
-func show_dice_face(face_number: int) -> void:
-	# Based on the animation using row 5 (frame_coords y=5)
-	# Dice faces 1-6 should be in row 5, columns 0-5
-	# For a 13 hframes x 14 vframes spritesheet:
-	# frame = y * hframes + x = 5 * 13 + (face_number - 1)
-	frame = 5 * 13 + (face_number - 1)  # Frames 65-70 for dice 1-6
+	face_anim.play("default")
+	face_anim.stop()
