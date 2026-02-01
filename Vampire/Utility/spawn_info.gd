4e22deeb81b7ba11fc74ce0374f0ae6a02315ee6
Mamadou Camara <mcamara4500@gmail.com>
2026-02-01 10:41:48 -0500

    Label hunting

--- Vampire/Utility/spawn_info.gd
+++ Vampire/Utility/spawn_info.gd
@@ -5,6 +5,6 @@ class_name Spawn_info
 @export var time_end: int = 999
 @export var monster: PackedScene       
 @export var monster_num: int = 1
-@export var monster_spawn_delay: int = 40
+@export var monster_spawn_delay: int = 5
 
 var spawn_delay_counter: int = 0
